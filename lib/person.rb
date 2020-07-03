f5289e8ebbef8ec962e40f7bf32d54d3757e6a2b
Deborah Santillan <s.debb@yahoo.com>
2020-07-03 15:26:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,18 @@
-def name=(dogs_name)
-    @name = dogs_name
+class Person
+ 
+  def name=(person_name)
+    @name = person_name
   end
  
   def name
     @name
+  end
+
+  def job=(person_job)
+    @job = person_job
+  end
+ 
+  def job
+    @job
   end
 end
